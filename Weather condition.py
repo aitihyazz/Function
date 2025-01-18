ad5e32c379276a12cc4988599ff70a88c2e78ad1
aitihyazz <Your GitHub Email>
2025-01-18 22:40:11 +0600

    Weather condition

---
+++ Weather condition.py
@@ -0,0 +1,7 @@
+spring = "autumn"
+autumn ="Winter" 
+def weather_condition():
+    print("Its decent in",spring)
+    print("Its same in",autumn)
+#you can plce it here too!!    
+weather_condition()
